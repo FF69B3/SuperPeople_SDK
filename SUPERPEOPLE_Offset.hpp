9e07c377981e76543e11c7908ef1c92dcfaf0ca2
FF69B4 <94247503+FF69B3@users.noreply.github.com>
2022-12-15 14:31:47 +0800

    SuperPeople
    update

---
+++ SUPERPEOPLE_Offset.hpp
@@ -0,0 +1,18 @@
+                                                      /* Create by #FF69B4 */
+#define ComponentVelocity  0xC0 //|size:0xC|type:struct FVector|class USceneComponent|
+#define RelativeRotation  0x1E0 //|size:0xC|type:struct FRotator|class USceneComponent|
+#define RootComponent  0x168 //|size:0x8|type:class USceneComponent*|class AActor|
+#define PlayerController  0x38 //|size:0x8|type:class APlayerController*|class UPlayer|
+#define PlayerState  0x348 //|size:0x8|type:class APlayerState*|class APawn|
+#define Mesh  0x3A8 //|size:0x8|type:class USkeletalMeshComponent*|class ACharacter|
+#define AcknowledgedPawn  0x478 //|size:0x8|type:class APawn*|class APlayerController|
+#define PlayerCameraManager  0x5C0 //|size:0x8|type:class APlayerCameraManager*|class APlayerController|
+#define ViewTarget  0x1090 //|size:0x690|type:struct FTViewTarget|class APlayerCameraManager|
+#define CameraCachePrivate  0x1DE0 //|size:0x680|type:struct FCameraCacheEntry|class APlayerCameraManager|
+#define LeanValue  0x1130 //|size:0x4|type:float|class ABravoHotelCharacter|
+#define Health  0x1CC8 //|size:0x4|type:float|class ABravoHotelCharacter|
+#define bIsFired  0x351C //|size:0x1|type:bool|class ABravoHotelCharacter|
+#define FPSK_SwayRotationIdle  0x3D2C //|size:0xC|type:struct FRotator|class ABravoHotelCharacter|
+#define TeamIndex  0x100 //|size:0x1|type:unsigned char|class UBHReplicatedPlayerInfo|
+#define ReplicatedPlayerInfo  0x5D0 //|size:0x8|type:class UBHReplicatedPlayerInfo*|class ABravoHotelPlayerState|
+#define CurrentState  0x6E9 //|size:0x1|type:EPlayerState|class ABravoHotelPlayerState|
